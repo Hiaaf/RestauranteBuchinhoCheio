32ee9ce56685cd5a20796f5456043c9b9878719f
Hiaaf <83770823+Hiaaf@users.noreply.github.com>
2023-11-11 18:00:24 -0300

    Delete src directory pq eu quero recomeçar

--- src/TesteRestaurante.java
+++
@@ -1,7 +0,0 @@
-public class TesteRestaurante {
-    public static void main(String[] args) {
-        var r1 = new Restaurante("Buchinho Cheio 1", "Av. 2", 5);
-
-
-    }
-}
--- src/UI.java
+++
@@ -1,6 +0,0 @@
-public class UI {
-    public static void comeco(Restaurante r) {
-        System.out.printf("Bem vindo ao programa de reserva do Restaurante %s!%n", r.getNome());
-        System.out.printf("");
-    }
-}
